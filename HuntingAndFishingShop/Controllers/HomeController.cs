f6e31e12a822962c30e66ddbb5e965c09f1e4bdb
roma-19 <122823516+roma-19@users.noreply.github.com>
2024-11-05 23:44:55 +0200

    add header and main page

--- HuntingAndFishingShop/Controllers/HomeController.cs
+++ HuntingAndFishingShop/Controllers/HomeController.cs
@@ -13,16 +13,11 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Main()
     {
         return View();
     }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
