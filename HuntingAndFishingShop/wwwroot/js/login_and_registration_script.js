e89894427cdd8066e1878e861a8ed01e51b66b24
roma-19 <122823516+roma-19@users.noreply.github.com>
2024-11-12 00:34:15 +0200

    add catalogue, feedbacks, login/registration form and footer

---
+++ HuntingAndFishingShop/wwwroot/js/login_and_registration_script.js
@@ -0,0 +1,45 @@
+﻿document.addEventListener('DOMContentLoaded',  function ()
+{
+    const signInBtn = document.querySelector('.signin-btn');
+    const signUpBtn = document.querySelector('.signup-btn');
+    const formBox = document.querySelector('.form-box');
+    const block = document.querySelector('.block');
+
+    if (signInBtn && signUpBtn) {
+        signUpBtn.addEventListener('click', function() {
+            formBox.classList.add('active');
+            block.classList.add('active');
+        });
+
+        signInBtn.addEventListener('click', function() {
+            formBox.classList.remove('active');
+            block.classList.remove('active');
+        });
+    }
+    function hiddenOpen_CloseClick()
+    {
+        let x = document.querySelector(".container-login-registration");
+        if (x.style.display === "none")
+        {
+            x.style.display = "grid";
+        }
+        else
+        {
+            x.style.display = "none";
+        }
+    }
+    
+    document.getElementById("click-to-hide").addEventListener("click", function(){
+        hiddenOpen_CloseClick();
+        formBox.classList.remove('active');
+        block.classList.remove('active');
+    });
+    document.getElementById("click-to-hide_1").addEventListener("click", function(){
+        hiddenOpen_CloseClick();
+        formBox.classList.add('active');
+        block.classList.add('active');
+});
+    document.querySelector(".overlay").addEventListener("click", hiddenOpen_CloseClick);
+});
+
+
